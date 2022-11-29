f8f0382584856a676b09397010abfc8edfeb5b62
mad_proxima <maxprognet@gmail.com>
2022-11-29 21:24:00 +0300

    Added topic and entry classes

--- 4startDjango.py
+++ 4startDjango.py
@@ -1,4 +1,5 @@
 # python -m venv ll_env - создаем venv
+# python3 -m venv --upgrade-deps fashion_venv - допом апгрейдим пип
 #
 # source ll_env/bin/activate - active venv
 #
@@ -13,3 +14,12 @@
 # python manage.py runserver -view project
 
 # python manage.py startapp learning_logs -создать инфраструктуру, необходимую для построения приложения
+
+# str1 = '\\dixy.local\dax\Bank\GPB\Выписки\2022\2022.11'
+# str2 = '\\dixy.local\dax\Bank\GPB\Выписки\2022\2022.11'
+#
+# if str1 == str2:
+#     print(True)
+#
+# python manage.py makemigrations
+# python manage.py migrate
--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model):
+    """Topic, which learn user"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Returned string view model"""
+        return self.text
+
+class Entry(models.Model):
+    """Information learned by the user on the topic"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Returned string view model"""
+        return f"{self.text[:50]}..."
